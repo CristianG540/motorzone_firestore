26def0410b04ed572645bda4a995d875a8219bc0
CristianG540 <cristianneatproy@gmail.com>
2018-07-26 12:13:49 -0500

    cambio en la funcion de utilidad de busqueda binaria que me ayuda a traer un valor exacto no el mas aproximado

--- src/providers/config/config.ts
+++ src/providers/config/config.ts
@@ -157,9 +157,19 @@ export class ConfigProvider {
     }, 60000)
   }
 
-  // Esta es una version mas rapida del "_.find" de lodash :3
-  // Gracias a https://pouchdb.com/2015/02/28/efficiently-managing-ui-state-in-pouchdb.html
-  static binarySearch (arr: any, property: string, search: any): number {
+  /**
+   * Esta es una version mas rapida del "_.find" de lodash :3
+   * Gracias a https://pouchdb.com/2015/02/28/efficiently-managing-ui-state-in-pouchdb.html
+   * @static
+   * @param {any[]} arr array de objetos donde voy a buscar wl valor
+   * @param {string} property propiedad del objeto que contiene el valor q busco
+   * @param {*} search lo que estoy buscando en el array
+   * @param {boolean} [strict] si este parametro es verdadero la busqueda me devuelve error
+   * si no encuentra el valor exacto que busco, de lo contrario me va a devolver el valor q mas se aproxime
+   * @returns {number}
+   * @memberof ConfigProvider
+   */
+  static binarySearch (arr: any[], property: string, search: any, strict?: boolean): number {
     let low: number = 0
     let high: number = arr.length
     let mid: number
@@ -167,6 +177,22 @@ export class ConfigProvider {
       mid = (low + high) >>> 1 // faster version of Math.floor((low + high) / 2)
       arr[mid][property] < search ? low = mid + 1 : high = mid
     }
+
+    /**
+     * lo que hago aqui es verificar si lo que busco en el array existe
+     * por ejemplo si un array de productos estoy buscando el codigo "MF3021"
+     * verifico que la posicion que me devuelve la funcion si sea la correcta
+     * si no hago esto, lo que va a pasar es que si el codigo no exste en el array de prodcutos
+     * la funcion me va a devolver el indice del codigo que mas se paresca
+     */
+    if (strict) {
+      if (arr[low][property] === search) {
+        return low
+      } else {
+        return 99999
+      }
+    }
+
     return low
   }
 
