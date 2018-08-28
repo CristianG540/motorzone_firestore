bfd029c942979fd66762c881312a2341474895e4
unknown <cristianneatproy@gmail.com>
2018-08-28 16:57:33 -0500

    agrego url cartera

--- src/providers/cartera/cartera.ts
+++ src/providers/cartera/cartera.ts
@@ -1,22 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { timeout } from 'rxjs/operators';
+import { Injectable } from '@angular/core'
+import { Storage } from '@ionic/storage'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { timeout } from 'rxjs/operators'
 
 // libs terceros
-import Raven from 'raven-js';
+import Raven from 'raven-js'
 
 // Providers
-import { ConfigProvider as cg } from '../config/config';
-import { AuthProvider } from '../../providers/auth/auth';
+import { ConfigProvider as cg } from '../config/config'
+import { AuthProvider } from '../../providers/auth/auth'
 
 @Injectable()
 export class CarteraProvider {
 
-  constructor(
+  constructor (
     private http: HttpClient,
     private storage: Storage,
-    private authServ: AuthProvider,
+    private authServ: AuthProvider
   ) {}
 
   /**
@@ -29,9 +29,9 @@ export class CarteraProvider {
    * @returns {Promise<any>}
    * @memberof CarteraProvider
    */
-  public async searchCartera(nitCliente: string): Promise<any> {
+  public async searchCartera (nitCliente: string): Promise<any> {
     try {
-      const token = await this.storage.get('josefa-token');
+      const token = await this.storage.get('josefa-token')
       /**
        * Bueno aqui hago todo lo contrario a lo que hago con los productos
        * en vez de hacer un offline first (que deberia ser lo correcto)
@@ -40,20 +40,20 @@ export class CarteraProvider {
        * traer digace fallo de conexion o lo que sea, entonces busco los clientes
        * en la base de datos local
        */
-      const url: string = cg.JOSEFA_URL + '/sap/cartera';
+      const url: string = cg.JOSEFA_URL + '/sap/cartera_motorzone'
       const options = {
         headers: new HttpHeaders({
           Accept: 'application/json',
           'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        }),
-      };
+          Authorization: 'Bearer ' + token
+        })
+      }
       const body: string = JSON.stringify({
         and: {
           codCliente: nitCliente,
-          codVendedor: this.authServ.userData.idAsesor,
-        },
-      });
+          codVendedor: this.authServ.userData.idAsesor
+        }
+      })
 
       /**
        * aqui haciendo uso del async/await hago un try/catch que primero
@@ -65,18 +65,18 @@ export class CarteraProvider {
       const res = await this.http
         .post(url, body, options)
         .pipe(timeout(7000))
-        .toPromise();
+        .toPromise()
 
-      return res;
+      return res
     } catch (err) {
-      console.error('Error searchCartera - providers/cartera.ts', err);
-      Raven.captureException( new Error(`Error searchCartera - providers/cartera.ts 🐛: ${JSON.stringify(err)}`), {
-        extra: err,
-      });
+      console.error('Error searchCartera - providers/cartera.ts', err)
+      Raven.captureException(new Error(`Error searchCartera - providers/cartera.ts 🐛: ${JSON.stringify(err)}`), {
+        extra: err
+      })
       throw new Error(
         'Error en cartera debido a un fallo con la conexion, verifique los datos o busque una red wifi: ' +
-          JSON.stringify(err),
-      );
+          JSON.stringify(err)
+      )
     }
   }
 }
